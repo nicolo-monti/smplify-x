f354be2a1f49282dc7ef056d84c187f0a71728e2
Sergey Prokudin <sergey.prokudin@gmail.com>
2021-04-09 00:02:10 +0200

    render func

--- smplifyx/fit_single_frame.py
+++ smplifyx/fit_single_frame.py
@@ -545,9 +545,11 @@ def render_mesh(out_mesh, camera_center, camera_transl, focal_length, img_width,
     mesh_new.vertex_colors = vertex_colors
     print("mesh visual kind: %s" % mesh_new.visual.kind)
 
-    mesh = pyrender.Mesh.from_trimesh(
-        mesh_new,
-        smooth=True, wireframe=False)
+    mesh_new = pyrender.Mesh.from_points(out_mesh.vertices, colors=colors)
+
+    #mesh = pyrender.Mesh.from_trimesh(
+    #    mesh_new,
+    #    smooth=True, wireframe=False)
 
     scene = pyrender.Scene(bg_color=[0.0, 0.0, 0.0, 0.0],
                            ambient_light=(0.3, 0.3, 0.3))
